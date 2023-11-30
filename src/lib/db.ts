1623f490f85102f248164f317f3f73fabe84e019
BORA <a52447879@gmail.com>
2023-11-30 23:50:23 +0900

    PostgreSQL + prisma

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+// Next.js를 사용하여 PrismaClient를 인스턴스화하는 모범 사례
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+export const db = prisma;
