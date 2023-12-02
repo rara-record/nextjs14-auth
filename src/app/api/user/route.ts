d66edcfef25dd1f99fbd2ba2d472f98d8017071b
BORA <a52447879@gmail.com>
2023-12-02 14:16:14 +0900

    feat: zod 유효성 검사

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,21 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
 import { hash } from 'bcrypt'
+import * as z from 'zod'
+
+const userSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  username: z.string().min(1, 'Username is required').max(100),
+  password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(8, 'Password must have than 8 characters'),
+})
 
 export const POST = async (request: Request) => {
   try {
     const body = await request.json()
-    const { email, username, password } = body
+    const { email, username, password } = userSchema.parse(body)
 
     // check if email already exists
     const existingUser = await db.user.findUnique({
@@ -13,7 +23,11 @@ export const POST = async (request: Request) => {
     })
 
     if (existingUser) {
-      return NextResponse.json({ user: null, message: '이미 가입한 유저입니다.', status: 409 })
+      return NextResponse.json({
+        user: null,
+        message: '이미 가입한 유저입니다.',
+        status: 409,
+      })
     }
 
     // 비밀번호 해싱
@@ -26,8 +40,15 @@ export const POST = async (request: Request) => {
 
     const { password: newUserPassword, ...rest } = newUser
 
-    return NextResponse.json({ user: rest, message: '회원가입에 성공했습니다.', status: 201 })
+    return NextResponse.json({
+      user: rest,
+      message: '회원가입에 성공했습니다.',
+      status: 201,
+    })
   } catch (e) {
-    return NextResponse.json({ error: e.message, status: 500 })
+    return NextResponse.json({
+      message: '유효한 요청이 아닙니다',
+      status: 500,
+    })
   }
 }
